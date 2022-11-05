ba5177931ff2f022046fb0fbd2e1f5df5d32a7f8
osamaOmrann <oo9264667@gmail.com>
2022-11-05 17:12:33 +0200

    Registeration Screen MVVM ✔

--- lib/ui/registeration/register_screen.dart
+++ lib/ui/registeration/register_screen.dart
@@ -1,7 +1,8 @@
 import 'package:find_me_ii/dialog_utils.dart';
 import 'package:find_me_ii/ui/log_in/login_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:find_me_ii/ui/registeration/register_viewModel.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../validation_utils.dart';
 
@@ -12,158 +13,158 @@ class RegisterScreen extends StatefulWidget {
   State<RegisterScreen> createState() => _RegisterScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen> {
+class _RegisterScreenState extends State<RegisterScreen>
+    implements RegisterNavigator {
   bool securePasswordI = true;
   bool securePasswordII = true;
   var formKey = GlobalKey<FormState>();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  late RegisterViewModel viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel = RegisterViewModel();
+    viewModel.navigator = this;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize:
-        Size.fromHeight(MediaQuery
-            .of(context)
-            .size
-            .height * .05),
-        child: AppBar(
-          shape: Theme
-              .of(context)
-              .appBarTheme
-              .shape,
-          centerTitle: true,
-          title: Text('Find Me'),
+    return ChangeNotifierProvider(
+      create: (_) => viewModel,
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize:
+              Size.fromHeight(MediaQuery.of(context).size.height * .05),
+          child: AppBar(
+            shape: Theme.of(context).appBarTheme.shape,
+            centerTitle: true,
+            title: Text('Find Me'),
+          ),
         ),
-      ),
-      body: Form(
-        key: formKey,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .12,
-                ),
-                TextFormField(
-                  validator: (text) {
-                    if (text == null || text.trim().isEmpty) {
-                      return 'User Name Is Required';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(labelText: 'User Name'),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .02,
-                ),
-                TextFormField(
-                  controller: emailController,
-                  validator: (text) {
-                    if (text == null || text
-                        .trim()
-                        .isEmpty) {
-                      return 'E-mail Adress Is Required';
-                    }
-                    if (!ValidationUtils.isValidEmail(text)) {
-                      return 'Pleas Enter A Valid E-mail';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(labelText: 'E-mail Adress'),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .02,
-                ),
-                TextFormField(
-                  controller: passwordController,
-                  validator: (text) {
-                    if (text == null || text
-                        .trim()
-                        .isEmpty) {
-                      return 'Please Enter Password';
-                    }
-                    if (text.length < 6) {
-                      return 'Passord Must Be At Least 6 Characters.';
-                    }
-                    return null;
-                  },
-                  obscureText: securePasswordI,
-                  decoration: InputDecoration(
-                      suffixIcon: InkWell(
-                          onTap: () {
-                            securePasswordI = !securePasswordI;
-                            setState(() {});
-                          },
-                          child: Icon(securePasswordI
-                              ? Icons.visibility
-                              : Icons.visibility_off)),
-                      labelText: 'Password'),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .02,
-                ),
-                TextFormField(
-                  validator: (text) {
-                    if (text == null || text
-                        .trim()
-                        .isEmpty) {
-                      return 'Please Confirm Password';
-                    }
-                    if (text != passwordController.text) {
-                      return 'The Two Passwords Are Not Identical.';
-                    }
-                    return null;
-                  },
-                  obscureText: securePasswordII,
-                  decoration: InputDecoration(
-                      suffixIcon: InkWell(
-                          onTap: () {
-                            securePasswordII = !securePasswordII;
-                            setState(() {});
-                          },
-                          child: Icon(securePasswordII
-                              ? Icons.visibility
-                              : Icons.visibility_off)),
-                      labelText: 'Confirm Password'),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .09,
-                ),
-                ElevatedButton(
-                  child: Text(
-                    'Create Account',
-                    style: Theme.of(context).textTheme.bodyMedium,
+        body: Form(
+          key: formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .12,
                   ),
-                  onPressed: () {
-                    createAccountClicked();
-                  },
-                  style: ButtonStyle(
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
-                            side: BorderSide(color: Colors.red))),
-                    backgroundColor: MaterialStateProperty.all(
-                        Theme.of(context).primaryColor),
-                    padding: MaterialStateProperty.all(
-                        EdgeInsets.symmetric(horizontal: 50, vertical: 15)),
-                  ),
-                ),
-                SizedBox(
-                  height: MediaQuery.of(context).size.height * .02,
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(
-                          context, LogInScreen.routeName);
+                  TextFormField(
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'User Name Is Required';
+                      }
+                      return null;
                     },
-                    child: Text('Already Have An Account?',
-                        style:
-                            TextStyle(color: Theme.of(context).primaryColor)))
-              ],
+                    decoration: InputDecoration(labelText: 'User Name'),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .02,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'E-mail Adress Is Required';
+                      }
+                      if (!ValidationUtils.isValidEmail(text)) {
+                        return 'Pleas Enter A Valid E-mail';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(labelText: 'E-mail Adress'),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .02,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please Enter Password';
+                      }
+                      if (text.length < 6) {
+                        return 'Passord Must Be At Least 6 Characters.';
+                      }
+                      return null;
+                    },
+                    obscureText: securePasswordI,
+                    decoration: InputDecoration(
+                        suffixIcon: InkWell(
+                            onTap: () {
+                              securePasswordI = !securePasswordI;
+                              setState(() {});
+                            },
+                            child: Icon(securePasswordI
+                                ? Icons.visibility
+                                : Icons.visibility_off)),
+                        labelText: 'Password'),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .02,
+                  ),
+                  TextFormField(
+                    validator: (text) {
+                      if (text == null || text.trim().isEmpty) {
+                        return 'Please Confirm Password';
+                      }
+                      if (text != passwordController.text) {
+                        return 'The Two Passwords Are Not Identical.';
+                      }
+                      return null;
+                    },
+                    obscureText: securePasswordII,
+                    decoration: InputDecoration(
+                        suffixIcon: InkWell(
+                            onTap: () {
+                              securePasswordII = !securePasswordII;
+                              setState(() {});
+                            },
+                            child: Icon(securePasswordII
+                                ? Icons.visibility
+                                : Icons.visibility_off)),
+                        labelText: 'Confirm Password'),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .09,
+                  ),
+                  ElevatedButton(
+                    child: Text(
+                      'Create Account',
+                      style: Theme.of(context).textTheme.bodyMedium,
+                    ),
+                    onPressed: () {
+                      createAccountClicked();
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: BorderSide(color: Colors.red))),
+                      backgroundColor: MaterialStateProperty.all(
+                          Theme.of(context).primaryColor),
+                      padding: MaterialStateProperty.all(
+                          EdgeInsets.symmetric(horizontal: 50, vertical: 15)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * .02,
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(
+                            context, LogInScreen.routeName);
+                      },
+                      child: Text('Already Have An Account?',
+                          style:
+                              TextStyle(color: Theme.of(context).primaryColor)))
+                ],
+              ),
             ),
           ),
         ),
@@ -171,21 +172,25 @@ class _RegisterScreenState extends State<RegisterScreen> {
     );
   }
 
-  var authService = FirebaseAuth.instance;
-
   void createAccountClicked() {
     if (formKey.currentState?.validate() == false) {
       return;
     }
-    showLoading(context, 'Loading..');
-    authService.createUserWithEmailAndPassword(
-        email: emailController.text, password: passwordController.text).then((
-        userCredential) {
-      hideLoading(context);
-      showMessage(context, userCredential.user?.uid ?? '');
-    }).onError((error, stackTrace) {
-      hideLoading(context);
-      showMessage(context, error.toString());
-    });
+    viewModel.register(emailController.text, passwordController.text);
+  }
+
+  @override
+  void showLoadingDialog({String message = 'Loading..'}) {
+    showLoading(context, message);
+  }
+
+  @override
+  void showMessageDialog(String message) {
+    showMessage(context, message);
+  }
+
+  @override
+  void hideLoadinDialog() {
+    hideLoading(context);
   }
 }
--- lib/ui/registeration/register_viewModel.dart
+++ lib/ui/registeration/register_viewModel.dart
@@ -1,1 +1,35 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 
+abstract class RegisterNavigator {
+  void showLoadingDialog({String message = 'Loading..'});
+
+  void hideLoadinDialog();
+
+  void showMessageDialog(String message);
+}
+
+class RegisterViewModel extends ChangeNotifier {
+  RegisterNavigator? navigator;
+  var auth = FirebaseAuth.instance;
+
+  void register(String email, String password) async {
+    navigator?.showLoadingDialog();
+    try {
+      var credentials = await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      navigator?.hideLoadinDialog();
+      navigator?.showMessageDialog(credentials.user?.uid ?? '');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        navigator?.showMessageDialog('Password Is Too Weak');
+      } else if (e.code == 'email-already-in-use') {
+        navigator?.showMessageDialog('Email Is Already Registered');
+      }
+    } catch (e) {
+      navigator?.hideLoadinDialog();
+      navigator
+          ?.showMessageDialog('Something Went Wrong. Please Try Again Later');
+    }
+  }
+}
