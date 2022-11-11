9deff6bd221e27c3207fb54ed27cc560373ab8d3
osamaOmrann <oo9264667@gmail.com>
2022-11-11 10:46:26 +0200

    Home Screen With 2 tabs (Home, and Settings) ✔

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -1,13 +1,27 @@
+import 'package:find_me_ii/base/base.dart';
 import 'package:find_me_ii/ui/home/home_side_menu.dart';
+import 'package:find_me_ii/ui/home/home_viewModel.dart';
 import 'package:find_me_ii/ui/home/settings_tab/settings_tab.dart';
 import 'package:flutter/material.dart';
 
 import 'home_tab/home_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const String routeName = 'Home Screen';
-  int selectedIndex = 0;
+  static int selectedIndex = 0;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends BaseState<HomeScreen, HomeViewModel>
+    implements HomeNavigator {
   List<Widget> tabs = [HomeTab(), SettingsTab()];
+
+  @override
+  HomeViewModel initViewModel() {
+    return HomeViewModel();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +30,29 @@ class HomeScreen extends StatelessWidget {
         child: HomeSideMenu(),
       ),
       appBar: AppBar(),
-      bottomNavigationBar: BottomAppBar(),
+      body: tabs[HomeScreen.selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: HomeScreen.selectedIndex,
+          onTap: (index) {
+            onTabClicked(index);
+            setState(() {});
+          },
+          items: [
+            BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon: Icon(Icons.home),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                backgroundColor:
+                    Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+                icon: Icon(Icons.settings),
+                label: 'Settings')
+          ]),
     );
+  }
+
+  void onTabClicked(index) {
+    viewModel.onTabSelected(index);
   }
 }
--- lib/ui/home/home_viewModel.dart
+++ lib/ui/home/home_viewModel.dart
@@ -1,31 +1,11 @@
+import 'package:find_me_ii/ui/home/home_screen.dart';
+
 import '../../base/base.dart';
 
-abstract class HomeNavigator extends BaseNavigator {
-  // void goToHome();
-}
+abstract class HomeNavigator extends BaseNavigator {}
 
 class HomeViewModel extends BaseViewModel<HomeNavigator> {
-  // var auth = FirebaseAuth.instance;
-
-  /*void login(String email, String password) async {
-    try {
-      navigator?.showLoadingDialog();
-      var credential = await auth.signInWithEmailAndPassword(
-          email: email, password: password);
-      var retrievedUser =
-      await MyDataBase.getUserById(credential.user?.uid ?? '');
-      navigator?.hideLoadinDialog();
-      if (retrievedUser == null) {
-        navigator?.showMessageDialog('Something Went Wrong. Try Again Later');
-      } else {
-        SharedData.user = retrievedUser;
-        navigator?.goToHome();
-      }
-    } on FirebaseAuthException catch (e) {
-      navigator?.hideLoadinDialog();
-      navigator?.showMessageDialog('Wrong User Name Or Password');
-    } catch (e) {
-      print(e);
-    }
-  }*/
+  void onTabSelected(index) {
+    HomeScreen.selectedIndex = index;
+  }
 }
