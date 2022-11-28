484c1bb2ddf79ad40e0db70b1d83d90b68df53ae
osamaOmrann <oo9264667@gmail.com>
2022-11-28 12:32:15 +0200

    Picking Image From Gallery ✔

--- lib/ui/home/home_tab/insert_missing_person_screen/pick_image_bottom_sheet.dart
+++ lib/ui/home/home_tab/insert_missing_person_screen/pick_image_bottom_sheet.dart
@@ -1,7 +1,19 @@
+import 'dart:io';
+
 import 'package:find_me_ii/my_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
 
-class PickImageBottomSheet extends StatelessWidget {
+class PickImageBottomSheet extends StatefulWidget {
+  @override
+  State<PickImageBottomSheet> createState() => _PickImageBottomSheetState();
+}
+
+class _PickImageBottomSheetState extends State<PickImageBottomSheet> {
+  final ImagePicker _picker = ImagePicker();
+  File? _image;
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -13,10 +25,8 @@ class PickImageBottomSheet extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          InkWell(
-            onTap: () {
-              capture(context);
-            },
+          GestureDetector(
+            onTap: captureImage,
             child: Image.asset(
               'assets/images/camera.png',
               width: 50,
@@ -24,23 +34,38 @@ class PickImageBottomSheet extends StatelessWidget {
             ),
           ),
           InkWell(
-            onTap: () {
-              pickFromGallery(context);
-            },
-            child: Image.asset(
-              'assets/images/picture.png',
-              width: 50,
-              height: 50,
-            ),
+            onTap: () => _pickImage(ImageSource.gallery),
+            child: _image == null
+                ? Image.asset(
+                    'assets/images/picture.png',
+                    width: 50,
+                    height: 50,
+                  )
+                : PickImageBottomSheet(),
           ),
         ],
       ),
     );
   }
 
-  Future capture(BuildContext context) async {}
+  void captureImage() async {
+    final XFile? image = await _picker.pickImage(source: ImageSource.camera);
+    setState(() {
+      _image = image as File?;
+    });
+  }
 
-  Future pickFromGallery(BuildContext context) async {
-    //ImagePicker().
+  Future _pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+      File? img = File(image.path);
+      setState(() {
+        _image = img;
+      });
+    } on PlatformException catch (e) {
+      print(e);
+      Navigator.of(context).pop();
+    }
   }
 }
