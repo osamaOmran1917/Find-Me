d4995475f363e321eb72c4684203bf42d4d3d7ed
osamaOmrann <oo9264667@gmail.com>
2022-12-17 01:05:37 +0200

    about to finish phone number validation ✔

--- lib/ui/registeration/phone_number_section/enter_phone_number_screen.dart
+++ lib/ui/registeration/phone_number_section/enter_phone_number_screen.dart
@@ -20,6 +20,7 @@ class _EnterPhoneNumberScreenState extends State<EnterPhoneNumberScreen> {
             padding: EdgeInsets.all(30),
             width: double.infinity,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.network(
                   'https://img.icons8.com/external-flaticons-lineal-color-flat-icons/2x/external-mobile-phone-resume-flaticons-lineal-color-flat-icons.png',
@@ -27,79 +28,99 @@ class _EnterPhoneNumberScreenState extends State<EnterPhoneNumberScreen> {
                   fit: BoxFit.cover,
                 ),
                 SizedBox(
-                  height: 50,
+                  height: MediaQuery.of(context).size.height * .05,
                 ),
                 FadeInDown(
                     child: Text(
-                  'Phone Number',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                )),
+                      'Phone Number',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    )),
                 FadeInDown(
                     child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  child: Text(
-                    'Enter your phone number to continue, we will send you OPT to verify.',
+                      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      child: Text(
+                        'Enter your phone number to continue, we will send you code to verify.',
                     textAlign: TextAlign.center,
                     style: TextStyle(color: Colors.grey.shade700),
                   ),
-                )),
+                    )),
                 SizedBox(
-                  height: 30,
+                  height: MediaQuery.of(context).size.height * .03,
                 ),
                 FadeInDown(
                     child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      boxShadow: [
-                        BoxShadow(
-                            color: Color(0xffeeeeee),
-                            blurRadius: 10,
-                            offset: Offset(0, 4))
-                      ],
-                      borderRadius: BorderRadius.circular(8),
-                      border:
+                      padding: EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                color: Color(0xffeeeeee),
+                                blurRadius: 10,
+                                offset: Offset(0, 4))
+                          ],
+                          borderRadius: BorderRadius.circular(8),
+                          border:
                           Border.all(color: Colors.black.withOpacity(0.13))),
-                  child: Stack(
-                    children: [
-                      InternationalPhoneNumberInput(
-                        onInputChanged: (value) {},
-                        cursorColor: Colors.black,
-                        formatInput: false,
-                        selectorConfig: SelectorConfig(
-                            selectorType: PhoneInputSelectorType.DIALOG),
-                        inputDecoration: InputDecoration(
-                            contentPadding:
+                      child: Stack(
+                        children: [
+                          InternationalPhoneNumberInput(
+                            onInputChanged: (value) {},
+                            cursorColor: Colors.black,
+                            formatInput: false,
+                            selectorConfig: SelectorConfig(
+                                selectorType: PhoneInputSelectorType.DIALOG),
+                            inputDecoration: InputDecoration(
+                                contentPadding:
                                 EdgeInsets.only(bottom: 15, left: 0),
-                            border: InputBorder.none,
-                            hintText: 'Phone Number',
-                            hintStyle: TextStyle(
-                                color: Colors.grey.shade500, fontSize: 16)),
-                      ),
-                      Positioned(
-                        left: 90,
-                        top: 8,
-                        bottom: 8,
-                        child: Container(
-                          height: 40,
+                                border: InputBorder.none,
+                                hintText: 'Phone Number',
+                                hintStyle: TextStyle(
+                                    color: Colors.grey.shade500, fontSize: 16)),
+                          ),
+                          Positioned(
+                            left: 90,
+                            top: 8,
+                            bottom: 8,
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * .04,
                           width: 1,
                           color: Colors.black.withOpacity(0.13),
                         ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                )),
+                    )),
                 SizedBox(
-                  height: 100,
+                  height: MediaQuery.of(context).size.height * .10,
                 ),
                 FadeInDown(
                     child: MaterialButton(
-                  onPressed: () {},
+                      onPressed: () {},
+                  color: Colors.black,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 30),
+                  minWidth: double.infinity,
                   child: Text(
-                    'Request OPT',
-                    style: TextStyle(),
+                    'Request Verification Code',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
                   ),
-                ))
+                )),
+                SizedBox(height: MediaQuery.of(context).size.height * .02),
+                FadeInDown(
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text(
+                              'Already have an account?',
+                              style: TextStyle(color: Colors.grey.shade700),
+                            ))
+                      ]),
+                )
               ],
             ),
           ),
